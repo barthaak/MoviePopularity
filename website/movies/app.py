0408400bae2a182f6857df5bf2cd2ae689b04647
Maarten Demandt <maartendemandt@gmail.com>
2018-11-29 22:53:20 +0100

    Cleaned the code and added comments

--- website/movies/app.py
+++ website/movies/app.py
@@ -1,24 +1,29 @@
 from flask import Flask, render_template, flash, redirect, url_for, session, request, logging, send_from_directory, Response
 import os
 
-#
+# Sete spark env variables
 os.environ[
     'PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.1 --master local[2] pyspark-shell'
 
 import findspark
 
+# Initialize spark location
 findspark.init()
 
 from pyspark.sql import SparkSession
+
+# Create spark sesstion
 mySpark = SparkSession.builder.appName("movieData").master("local").getOrCreate()
+# Read movies from database
 movies = mySpark.read.format("com.mongodb.spark.sql.DefaultSource") \
           .option("uri","mongodb://localhost:27017/movieDatabase.movies").load()
+
+# Create hashtag list
 hashtag_list = []
 for h in movies.select('title_hashtag').toPandas()['title_hashtag']: # get all documents
     hashtag_list.append(h)
 
 movies_dict = {el:0 for el in hashtag_list}
-#
 
 from kafka import KafkaConsumer, TopicPartition
 import schedule
@@ -26,6 +31,7 @@ import time
 
 app = Flask(__name__)
 
+# Initialize kafka consumer and subscribe to tweets analysis topic
 consumer = KafkaConsumer(bootstrap_servers='localhost:9092',
                                      auto_offset_reset='earliest')
 consumer.subscribe(['tweets_analysis'])
@@ -41,24 +47,24 @@ def about():
 @app.route('/dashboard', methods=['GET', 'POST'])
 def dashboard():
     return Response(kafkastream(), mimetype='text/plain')
-    #activate_stream_job().apply_async()
 
 def kafkastream():
+    # Foreach message in the consumer
     for msg in consumer:
+        # Get data from message
         result = ''
         movie_hashtag = msg.value.decode('utf-8').split(" ")[0]
         movie_count = int(msg.value.decode('utf-8').split(" ")[1])
         movies_dict[movie_hashtag] = movie_count
-
+        
+        # Create sorted movie list, where count is desc
         sorted_movies = sorted(movies_dict.items(), key=lambda x: x[1], reverse=True)
         for x in sorted_movies:
             result += str(x[0]) + ' ' + str(x[1]) + '\n'
 
         result += '\n\n'
+        # Print the resulting table
         yield(result)
-        #yield(msg.value.decode('utf-8') + '\n')
-        #yield(msg.value.decode('utf-8').split(" ")[0] + '\t' + msg.value.decode('utf-8').split(" ")[1] + '\n')
 
 if __name__ == '__main__':
-#     app.secret_key='secret123'
     app.run(debug=True)
